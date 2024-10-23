8302263373aedd81f8e623c5fbaa0cd8d5de4939
vivek-tripathi-9005 <vivektripathi8005@gmail.com>
2024-10-23 22:38:59 +0530

    style: add buttons in home page
    - Added below links to base url page:
        - [Continue with Google] to redirect to /auth/google
            - [Continue with Outlook] to redirect to /auth/outlook

--- src/app.service.ts
+++ src/app.service.ts
@@ -3,6 +3,7 @@ import { Injectable } from '@nestjs/common';
 @Injectable()
 export class AppService {
   getHello(): string {
-    return 'Hello World!';
+    return `<p><a href="/auth/google" role="button"> Continue with Google </a></p> 
+        <p><a href="/auth/outlook" role="button"> Continue with Outlook </a></p>`;
   }
 }
