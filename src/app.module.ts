6b9902e49d9f6678e4f59cf23918dbd08fdbd0a3
vivek-tripathi-9005 <vivektripathi8005@gmail.com>
2024-10-23 19:51:06 +0530

    feat: create service and module for email and injected into main app module: - Used nest generate command to auto generate module and service for email

--- src/app.module.ts
+++ src/app.module.ts
@@ -3,11 +3,13 @@ import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { AuthModule } from './auth/auth.module';
 import { ConfigModule } from '@nestjs/config';
+import { EmailModule } from './email/email.module';
 
 @Module({
   imports: [
     ConfigModule.forRoot(),
-    AuthModule
+    AuthModule,
+    EmailModule
   ],
   controllers: [AppController],
   providers: [AppService],
