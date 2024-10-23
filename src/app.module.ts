c2970df51d50d16152e44603bec141ef791a6b22
vivek-tripathi-9005 <vivektripathi8005@gmail.com>
2024-10-23 21:49:47 +0530

    feat: configure Bull module to connect to Redis in the application

--- src/app.module.ts
+++ src/app.module.ts
@@ -5,11 +5,18 @@ import { AuthModule } from './auth/auth.module';
 import { ConfigModule } from '@nestjs/config';
 import { EmailModule } from './email/email.module';
 import { OpenaiModule } from './openai/openai.module';
+import { BullModule } from '@nestjs/bull';
 
 @Module({
   imports: [
     ConfigModule.forRoot(),
     AuthModule,
+    BullModule.forRoot({
+      redis: {
+        host: 'localhost',
+        port: 6379,
+      },
+    }),
     EmailModule,
     OpenaiModule
   ],
