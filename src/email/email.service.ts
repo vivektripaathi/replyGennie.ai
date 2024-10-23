ea648a108f33e658437c91443b05199f1f910d2e
vivek-tripathi-9005 <vivektripathi8005@gmail.com>
2024-10-23 21:18:28 +0530

    feat: add Gmail client initialization and OpenAI service integration to EmailService

--- src/email/email.service.ts
+++ src/email/email.service.ts
@@ -1,4 +1,25 @@
-import { Injectable } from '@nestjs/common';
+import { InjectQueue } from '@nestjs/bull';
+import { Injectable, Logger } from '@nestjs/common';
+import { Queue } from 'bull';
+import { google } from 'googleapis';
+import { OpenaiService } from 'src/openai/openai.service';
 
 @Injectable()
-export class EmailService {}
+export class EmailService {
+    private readonly logger = new Logger(EmailService.name);
+    private gmailClient;
+
+    constructor(
+        @InjectQueue('emailQueue') private emailQueue: Queue,
+        private readonly openaiService: OpenaiService
+    ) {}
+
+    initializeGmailClient(accessToken: string) {
+        const oauth2Client = new google.auth.OAuth2();
+        oauth2Client.setCredentials({ access_token: accessToken });
+        this.gmailClient = google.gmail({
+            version: 'v1',
+            auth: oauth2Client,
+        });
+    }
+}
