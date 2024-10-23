506a6bdf419e9e8eaf15f50fa776a57a79b6a3e1
vivek-tripathi-9005 <vivektripathi8005@gmail.com>
2024-10-23 21:29:33 +0530

    feat: enhance EmailModule with Bull queue integration and EmailProcessor

--- src/email/email.module.ts
+++ src/email/email.module.ts
@@ -1,7 +1,16 @@
 import { Module } from '@nestjs/common';
 import { EmailService } from './email.service';
+import { BullModule } from '@nestjs/bull';
+import { EmailProcessor } from './email.processor';
+import { OpenaiService } from '../openai/openai.service'; // Update this if necessary based on your project structure
 
 @Module({
-  providers: [EmailService]
+    imports: [
+        BullModule.registerQueue({
+            name: 'emailQueue',
+        }),
+    ],
+    providers: [EmailService, EmailProcessor, OpenaiService],
+    exports: [EmailService],
 })
 export class EmailModule {}
