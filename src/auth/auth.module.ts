e7a713b04dbf0a7aa1e80c9fb5b599d54b57aae9
vivek-tripathi-9005 <vivektripathi8005@gmail.com>
2024-10-23 21:50:18 +0530

    chore: imported email module in auth module

--- src/auth/auth.module.ts
+++ src/auth/auth.module.ts
@@ -4,9 +4,10 @@ import { AuthController } from './auth.controller';
 import { GoogleStrategy } from './strategies/google.strategy';
 import { OutlookStrategy } from './strategies/outlook.strategy';
 import { ConfigModule } from '@nestjs/config';
+import { EmailModule } from 'src/email/email.module';
 
 @Module({
-  imports: [PassportModule, ConfigModule],
+  imports: [PassportModule, ConfigModule, EmailModule],
   controllers: [AuthController],
   providers: [GoogleStrategy, OutlookStrategy],
 })
